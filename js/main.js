e0ab154fd5624d372f8ae88c67a3b666c4e433e6
Pham Van Tin <phamvantin.work@gmail.com>
2022-05-24 10:25:20 +0700

    Add and edit interceptor
    - Attach token to request if exists
    - Transform data for all responses

--- js/main.js
+++ js/main.js
@@ -1,10 +1,18 @@
 import axiosClient from './api/axiosClient'
-console.log(axiosClient)
+import postApi from './api/postApi'
+
 console.log('Hello')
 
 async function main() {
-  const respone = await axiosClient.get('/posts')
-  console.log(respone)
+  // const respone = await axiosClient.get('/posts')
+  try {
+    const queryParams = {
+      _page: 1,
+      _limit: 5,
+    }
+    const data = await postApi.getAll(queryParams)
+    console.log(data)
+  } catch (error) {}
 }
 
 main()
